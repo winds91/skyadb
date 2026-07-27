87f741504d151826a55966ca67b21d4a10c95ecf
杨圣飞 <yangshengfei330@qq.com>
2026-07-27 15:51:09 +0800

    旧版本优化

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,16 +1,21 @@
 pluginManagement {
     repositories {
+		maven { url = uri("https://maven.aliyun.com/repository/google") }
+        maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin") }
+        maven { url = uri("https://maven.aliyun.com/repository/public") }
+        maven { url = uri("https://maven.aliyun.com/repository/jcenter") }
         google()
-        mavenCentral()
-        gradlePluginPortal()
     }
 }
 
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
+        maven { url = uri("https://maven.aliyun.com/repository/google") }
+        maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin") }
+        maven { url = uri("https://maven.aliyun.com/repository/public") }
+        maven { url = uri("https://maven.aliyun.com/repository/jcenter") }
         google()
-        mavenCentral()
         maven("https://jitpack.io")
     }
 }
